18dcbea3d806afb8a91638ad68101f56d8977f32
marilianunesalves <nunes.marilia@gmail.com>
2022-05-25 15:43:24 -0300

    Initial commit

---
+++ src/main/java/com/exam/financial/ExamFinancialAccountApplication.java
@@ -0,0 +1,13 @@
+package com.exam.financial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamFinancialAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamFinancialAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exam/financial/ExamFinancialAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.financial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamFinancialAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
